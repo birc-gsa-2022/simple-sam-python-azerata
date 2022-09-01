9de45a44fc0364f9beb46f58344d7dfbaab0b324
azerata <smedemarkhammer@gmail.com>
2022-09-01 13:56:42 +0200

    Changed newline character

--- src/tosam.py
+++ src/tosam.py
@@ -11,8 +11,8 @@ def main():
 
     for line in args.mas:
         chrom, read_name, read_str, pos = line.split('\t')
-        print(read_name.strip(), chrom.strip(), pos.strip(),
-              f'{len(read_str)}M', read_str.strip(), sep='\t', end='\n')
+        print(read_name.strip(), chrom.strip(), int(pos.strip())+1,
+              f'{len(read_str)}M', read_str.strip(), sep='\t', end='\r\n')
 
 
 if __name__ == '__main__':
